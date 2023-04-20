43289f07d98c6401b3f08908d72f08d72657f7b0
resojmc <resojmc@gmail.com>
2023-04-19 20:08:23 -0700

    Update age calculator.py

--- age calculator.py
+++ age calculator.py
@@ -1,32 +1,36 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("300x600")
+root.geometry("300x200")
 root.title("Age Calculator")
 
 def find_age():
     year = int(main_entry.get())
-    age = 2023 - year
-    return age
-
+    month = int(second_entry.get())
+    years_old = 2023 - year
+    return years_old, month
+    
 def update_age():
     age = find_age()
-    if age >= 20 and age < 30:
-        result_label.config(text=f"You're {age} years old! Not a teenager anymore!")
-    elif age >= 30 and age < 40:
-        result_label.config(text=f"You're {age} years old! How's Life?")
-    elif age >= 40 and age < 60:
-        result_label.config(text=f"You're {age} years young! Stay in shape!")
-    elif age > 60:
-        result_label.config(text=f"You're {age} years old! Wow!")
+    y = age[0]
+    m = age[1]
+    if y < 1 and m < 12:
+        result_label.config(text=f"Aww, You're only {m} months old.")
     else:
-        result_label.config(text="")
+        result_label.config(text=f"You're {y} years and {m} months old!")
 
 main_label = tk.Label(root, text="Enter Birth Year")
 main_label.pack()
 
 main_entry = tk.Entry(root,)
 main_entry.pack()
+
+second_label = tk.Label(root, text="""Enter Birth Month Number
+eg: Jan=1, Feb=2 etc..""")
+second_label.pack()
+
+second_entry = tk.Entry(root,)
+second_entry.pack()
 
 button = tk.Button(root, text="Go", command=update_age)
 button.pack()
